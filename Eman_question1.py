d90dd7ecafd2e46c5318d74a7ca97f4e3b03f756
eman sheikh <emansheikhmas@gmail.com>
2026-09-10 00:54:03 +0500

    Add files via upload

---
+++ Eman_question1.py
@@ -0,0 +1,8 @@
+# Question 1: Greeting Program
+# Write a python program that takes the user's name as an input and then prints a greeting message like "Hello Ali.
+def main():
+    name = input("Enter Your Name: ")
+    print(f"Hello {name}")
+
+if __name__ == "__main__":
+    main()
